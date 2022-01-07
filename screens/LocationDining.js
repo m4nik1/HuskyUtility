9f58800c9788f249eadffcca827d002fbf4bc3cf
m4nik1 <maniksoomro@hotmail.com>
2022-01-07 17:52:03 -0500

    Working on location...

--- screens/LocationDining.js
+++ screens/LocationDining.js
@@ -1,38 +1,50 @@
 import React, { useEffect, useState } from "react"
 
-import {View, StyleSheet, Dimensions} from "react-native"
+import {View, StyleSheet, Dimensions, Text} from "react-native"
 import * as Location from "expo-location";
-import MapView from "react-native-maps";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import moment from "moment";
 
 
 
 const LocationDining = () => {
-    
-    const[Location, setLocation] = useState(null);
     const[errorMsg, setErrorMsg] = useState(null);
     const [mapRegion, setMapRegion] = useState();
 
+    let location2 = {
+        latitude: 23.259933,
+        longitude: 77.412613,
+        latitudeDelta: 0.009,
+        longitudeDelta: 0.009
+    }
+
+    async function getCurrentLocation() {
+        const { status } = await Location.requestForegroundPermissionsAsync()
+        if(status !== 'granted') {
+            setErrorMsg("Permission to access location is denied");
+            return;
+        }
+
+        let location = await Location.getCurrentPositionAsync({})
+        console.log(location)
+    }
+
     useEffect(() => {
-        (async () => {
-            let { status } = await Location.requestForegroundPermissionsAsync();
-            if(status !== 'granted') {
-                setErrorMsg("Permission to access location was denied");
-                return;
-            }
-
-            let location = await Location.getCurrentPositionAsync({});
-
-            let regionLocation = {latitude: location["coords"]["latitude"], longitude: location["coords"]["longitude"],
-                                  latitudeDelta:0.0922, longitudeDelta:0.0421}
-
-            setLocation(location);
-            setMapRegion(regionLocation);
-        })();
+        getCurrentLocation();
     }, [])
     
     return (
         <View>
-            <MapView style={styles.map} />
+            <MapView 
+                style={styles.map} 
+                provider={PROVIDER_GOOGLE}
+                // region={mapRegion}
+                followsUserLocation={true}
+                zoomEnabled={true}
+                showsUserLocation={true}
+
+
+            />
         </View>
     )
 }
