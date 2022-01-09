31101c40228d72228fcce919ffd05836ce3348d7
m4nik1 <maniksoomro@hotmail.com>
2022-01-08 22:12:52 -0500

    Modal is working

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Button } from 'react-native';
 import Home from './screens/HomeScreen';
 import Home2 from './screens/HomeScreen2';
 import moment from "moment";
 import LocationDining from './screens/LocationDining';
+import ModalDining from './components/diningHallModal';
 
 export default function App() {
 
   const [screen, setScreen] = useState("home")
+  const [modal, setModal] = useState(false)
 
   const day = moment().format('dddd');
 
@@ -21,6 +23,8 @@ export default function App() {
     <SafeAreaView style={styles.container}>
       <Home current_day={day} shouldRengar={screen === "classes"} changeScreen={(screen2) => {changeScreen(screen2)}} />
       <Home2 currentDay={day} shouldRengar={screen === "home"} changeScreen={(screen2) => {changeScreen(screen2)}} />
+      <Button title='Show Modal' onPress={() => setModal(true)} />
+      <ModalDining isVisible={modal} modalCancel={() => setModal(false)} />
     </SafeAreaView>
 
     // <View>
--- components/diningHallModal.js
+++ components/diningHallModal.js
@@ -1,17 +1,37 @@
 import React from "react";
-import { View, Modal, StyleSheet, Text } from "react-native"
+import { View, Modal, StyleSheet, Text, Button } from "react-native"
+import Card from "./Card";
 
 const ModalDining = props => {
-    return (
-        <View>
-            <Modal>
-                <View>
-                    <Text>Dining Hall Title</Text>
-                    <Text>Menu</Text>
-                </View>
-            </Modal>
-        </View>
-    );
+
+    function cancelModal() {
+        props.modalCancel(false)
+    }
+
+    if(props.isVisible) {
+        return (
+            <View>
+                <Modal animationType="slide" visible={props.isVisible} transparent={true}>
+                    <Card style={styles.modalView}>
+                        <Text>Dining Hall Title</Text>
+                        <Text>Menu</Text>
+                        <Button title='Exit' onPress={() => cancelModal()} />
+                    </Card>
+                </Modal>
+            </View>
+        );
+    }
+    else {
+        return (
+            null
+        )
+    }
 }
+
+const styles= StyleSheet.create({
+    modalView: {
+        marginTop: 700
+    }
+})
 
 export default ModalDining;
