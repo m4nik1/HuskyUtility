536e9bf196eb9c7412d3863903e7e9ee697da799
Manik Soomro <maniksoomro@Maniks-MacBook-Pro-2.local>
2022-01-19 17:13:48 -0500

    Can read status now

--- data/UConnDining/DiningHallStatus.js
+++ data/UConnDining/DiningHallStatus.js
@@ -1,20 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import * as cheerio from 'cheerio'
 import axios from "axios"
 import { Image, View, StyleSheet, Dimensions, Text, Alert, Button} from "react-native"
 
 const DiningHallStatus = () => {
-    const baseURL = ""
+    const baseURL = "http://localhost:8081"
+
+    const [status, ChangeStatus] = useState()
 
     async function fetchHtmlData() {
         axios.get(baseURL)
             .then(res => {
                 console.log(res.data)
+                ChangeStatus(res.data)
             })
     }
 
+    useEffect(() => {
+        fetchHtmlData()
+    }, [])
+
     return (
-        <Button title='Press this' onPress={() => fetchHtmlData()} />
+        // <Button title='Press this' onPress={() => fetchHtmlData()} />
+        <View>
+            <Text style={ (status === 'Closed') ? {color: 'red'} : {color: 'green'} }>{status}</Text>
+        </View>
     )
 
 }
