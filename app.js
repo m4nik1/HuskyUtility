3a1e4c615c72c897bf61a92c4e85b136437cba3b
m4nik1 <maniksoomro@hotmail.com>
2022-01-18 21:47:57 -0500

    Scraping status from dining halls

---
+++ app.js
@@ -0,0 +1,37 @@
+const http = require('http')
+const axios = require('axios');
+const cheerio = require('cheerio')
+const port = 8081
+
+async function diningScrape() {
+    let toScrape;
+
+    await axios.get('http://nutritionanalysis.dds.uconn.edu/shortmenu.aspx?sName=UCONN+Dining+Services&locationNum=03&locationName=Buckley+Dining+Hall&naFlag=1&WeeksMenus=This+Week%27s+Menus&myaction=read&dtdate=1%2f18%2f2022')
+        .then(res => {
+            console.log("We are scraping")
+            toScrape = res.data
+        })
+
+        .catch(err => {
+            console.log(err)
+    })
+    // console.log(toScrape)
+    const $ = cheerio.load(toScrape)
+
+    console.log($('.shortmenuinstructs').text())
+
+
+}
+
+
+const server = http.createServer(function (req, res) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+})
+
+server.listen(port, '127.0.0.1', () => {
+    console.log(`Server is running`);
+    diningScrape()
+})
+
