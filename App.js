d77dfe8b2d9143667d8deb5b3d0c6e8523c61626
m4nik1 <maniksoomro@hotmail.com>
2022-05-14 01:21:40 -0400

    Added back button

--- App.js
+++ App.js
@@ -79,7 +79,7 @@ export default function App() {
         screenOptions={{ 
           tabBarStyle: { position: 'absolute' }, tabBarBackground: () => (
           <BlurView tint="light" intensity={100} style={StyleSheet.absoluteFill} />), }} 
-        initialRouteName="Home" tabBar={(props) => <TabBar {... props} />} >
+        initialRouteName="Home" >
         <Tab.Screen name="Home" component={MainScreen} options={ {headerShown: false} } />
         <Tab.Screen name="Dining-Maps" component={LocationDining} options={ {headerShown: false} } />
         <Tab.Screen name="Classes" component={Home} options={ {headerShown: false} } />
