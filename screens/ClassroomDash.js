6e16934e2813bb011aaf374197af9923abded8d1
m4nik1 <maniksoomro@hotmail.com>
2022-08-12 04:49:16 -0400

    Added Classroom Space screen

---
+++ screens/ClassroomDash.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
+
+const ClassroomDash = props => {
+    return (
+        <View>
+            <Text>
+                Welcome to classroom space!
+            </Text>
+        </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        alignContent: 'center'
+    }
+})
+
+export default ClassroomDash;
