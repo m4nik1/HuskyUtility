1b21d371daf8216162e6a14bd677ea8c9db521d5
m4nik1 <maniksoomro@hotmail.com>
2022-08-17 23:36:10 -0400

    Dining dashboard

---
+++ screens/DiningDashboard.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+
+import { View, Text, ScrollView, StyleSheet, FlatList, Button, TouchableOpacity } from "react-native";
+
+const DiningDashboard = () => {
+    
+}
+
+export default DiningDashboard;
